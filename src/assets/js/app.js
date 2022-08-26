a0595e212d34b7afe13638028451bf8ee51df983
StreveSuksess <107485313+StreveSuksess@users.noreply.github.com>
2022-08-26 12:36:28 +0500

    Delete src directory

--- src/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-let nav = document.getElementById("header__nav");
-let burger = document.getElementById("burger");
-burger.addEventListener("click", () => nav.classList.toggle("show"));
-
-let title = document.getElementsByTagName("title")[0].innerHTML;
-let activeLinks = [document.getElementById("header" + title), document.getElementById("footer" + title)]
-activeLinks.forEach((elem) => elem.classList.add("nav__link--active"));
